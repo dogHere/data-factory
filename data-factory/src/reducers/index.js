684027a5a3b3c22cf96495792f48cbd47cdffa27
duck <duck@ducks-MBP.lan>
2018-07-03 12:30:58 +0800

    refactor(all): clean codes

--- data-factory/src/reducers/index.js
+++ data-factory/src/reducers/index.js
@@ -1,13 +1,7 @@
 import { combineReducers } from 'redux'
-import login from './login'
-import user from './users'
-import common from './common'
 import dataSource from './dataSource';
 
 const app = combineReducers({
-  login,
-  user,
-  common,
   dataSource
 })
 
